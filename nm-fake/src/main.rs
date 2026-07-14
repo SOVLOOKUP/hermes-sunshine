d2a525a402524709753c35a6bfca7f4bc84ced56
sovlookup <gonorth@qq.com>
2026-07-15 00:19:40 +0800

    fix: 添加IP4Config和IP6Config D-Bus接口，修复网络图标灰色问题
    - 桌面小程序查询ActiveConnection的IP配置时不再失败

--- nm-fake/src/main.rs
+++ nm-fake/src/main.rs
@@ -5,6 +5,8 @@ use zvariant::ObjectPath;
 const BUS_NAME: &str = "org.freedesktop.NetworkManager";
 const OBJECT_PATH: &str = "/org/freedesktop/NetworkManager";
 const ACTIVE_CONNECTION_PATH: &str = "/org/freedesktop/NetworkManager/ActiveConnection/0";
+const IP4_CONFIG_PATH: &str = "/org/freedesktop/NetworkManager/IP4Config/0";
+const IP6_CONFIG_PATH: &str = "/org/freedesktop/NetworkManager/IP6Config/0";
 
 struct NetworkManager;
 
@@ -84,12 +86,112 @@ impl ActiveConnection {
     }
 }
 
+struct IP4Config;
+
+#[interface(name = "org.freedesktop.NetworkManager.IP4Config")]
+impl IP4Config {
+    fn addresses(&self) -> Vec<(u32, u32, ObjectPath<'_>)> {
+        Vec::new()
+    }
+
+    fn gateways(&self) -> Vec<(i32, u32, ObjectPath<'_>)> {
+        Vec::new()
+    }
+
+    fn nameservers(&self) -> Vec<u32> {
+        Vec::new()
+    }
+
+    fn domains(&self) -> Vec<&str> {
+        Vec::new()
+    }
+
+    fn wins_servers(&self) -> Vec<u32> {
+        Vec::new()
+    }
+
+    fn routes(&self) -> Vec<(u32, u32, u32, i32, ObjectPath<'_>)> {
+        Vec::new()
+    }
+
+    fn gateway(&self) -> u32 {
+        0
+    }
+
+    fn method(&self) -> u32 {
+        2
+    }
+
+    fn is_gateway(&self) -> bool {
+        false
+    }
+
+    fn dns_servers(&self) -> Vec<u32> {
+        Vec::new()
+    }
+
+    fn dns_search(&self) -> Vec<&str> {
+        Vec::new()
+    }
+}
+
+struct IP6Config;
+
+#[interface(name = "org.freedesktop.NetworkManager.IP6Config")]
+impl IP6Config {
+    fn addresses(&self) -> Vec<(Vec<u8>, u32, i32, ObjectPath<'_>)> {
+        Vec::new()
+    }
+
+    fn gateways(&self) -> Vec<(i32, u32, ObjectPath<'_>)> {
+        Vec::new()
+    }
+
+    fn nameservers(&self) -> Vec<Vec<u8>> {
+        Vec::new()
+    }
+
+    fn domains(&self) -> Vec<&str> {
+        Vec::new()
+    }
+
+    fn routes(&self) -> Vec<(Vec<u8>, u32, Vec<u8>, i32, ObjectPath<'_>)> {
+        Vec::new()
+    }
+
+    fn gateway(&self) -> Vec<u8> {
+        Vec::new()
+    }
+
+    fn method(&self) -> u32 {
+        2
+    }
+
+    fn is_gateway(&self) -> bool {
+        false
+    }
+
+    fn dns_servers(&self) -> Vec<Vec<u8>> {
+        Vec::new()
+    }
+
+    fn dns_search(&self) -> Vec<&str> {
+        Vec::new()
+    }
+
+    fn dhcp_iaid(&self) -> u32 {
+        0
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let _conn = connection::Builder::system()?
         .name(BUS_NAME)?
         .serve_at(OBJECT_PATH, NetworkManager)?
         .serve_at(ACTIVE_CONNECTION_PATH, ActiveConnection)?
+        .serve_at(IP4_CONFIG_PATH, IP4Config)?
+        .serve_at(IP6_CONFIG_PATH, IP6Config)?
         .build()
         .await?;
 
