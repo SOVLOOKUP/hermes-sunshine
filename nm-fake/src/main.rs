3b1a09b1234d095e1b28bf332f6757a00a1bf8ce
sovlookup <gonorth@qq.com>
2026-07-15 01:19:57 +0800

    fix: 将D-Bus方法改为属性，修复Steam网络图标灰色
    - zbus默认暴露为D-Bus方法，Steam通过Properties.Get读取属性
    
    - 添加#[zbus(property)]属性标记
    
    - 添加primary_connection_type和ActiveConnection.type属性
    
    - 简化IP4Config/IP6Config，只保留关键属性

--- nm-fake/src/main.rs
+++ nm-fake/src/main.rs
@@ -13,44 +13,59 @@ struct NetworkManager;
 
 #[interface(name = "org.freedesktop.NetworkManager")]
 impl NetworkManager {
+    #[zbus(property)]
     fn networking_enabled(&self) -> bool {
         true
     }
 
+    #[zbus(property)]
     fn wireless_enabled(&self) -> bool {
         false
     }
 
+    #[zbus(property)]
     fn wireless_hardware_enabled(&self) -> bool {
         false
     }
 
+    #[zbus(property)]
     fn wwan_enabled(&self) -> bool {
         false
     }
 
+    #[zbus(property)]
     fn wwan_hardware_enabled(&self) -> bool {
         false
     }
 
+    #[zbus(property)]
     fn active_connections(&self) -> Vec<ObjectPath<'_>> {
         vec![ObjectPath::try_from(ACTIVE_CONNECTION_PATH).unwrap()]
     }
 
+    #[zbus(property)]
     fn primary_connection(&self) -> ObjectPath<'_> {
         ObjectPath::try_from(ACTIVE_CONNECTION_PATH).unwrap()
     }
 
+    #[zbus(property)]
     fn state(&self) -> u32 {
         70
     }
 
+    #[zbus(property)]
     fn connectivity(&self) -> u32 {
         4
     }
 
+    #[zbus(property)]
     fn devices(&self) -> Vec<ObjectPath<'_>> {
         vec![ObjectPath::try_from(WIRED_DEVICE_PATH).unwrap()]
+    }
+
+    #[zbus(property)]
+    fn primary_connection_type(&self) -> &str {
+        "802-3-ethernet"
     }
 }
 
@@ -58,34 +73,47 @@ struct ActiveConnection;
 
 #[interface(name = "org.freedesktop.NetworkManager.Connection.Active")]
 impl ActiveConnection {
+    #[zbus(property)]
     fn id(&self) -> &str {
         "Docker Network"
     }
 
+    #[zbus(property)]
     fn uuid(&self) -> &str {
         "docker-network-uuid"
     }
 
+    #[zbus(property)]
+    fn type_(&self) -> &str {
+        "802-3-ethernet"
+    }
+
+    #[zbus(property)]
     fn connection(&self) -> ObjectPath<'_> {
         ObjectPath::try_from("/org/freedesktop/NetworkManager/Settings/0").unwrap()
     }
 
+    #[zbus(property)]
     fn devices(&self) -> Vec<ObjectPath<'_>> {
-        Vec::new()
+        vec![ObjectPath::try_from(WIRED_DEVICE_PATH).unwrap()]
     }
 
+    #[zbus(property)]
     fn state(&self) -> u32 {
         2
     }
 
+    #[zbus(property)]
     fn default(&self) -> bool {
         true
     }
 
+    #[zbus(property)]
     fn ipv4_config(&self) -> ObjectPath<'_> {
         ObjectPath::try_from("/org/freedesktop/NetworkManager/IP4Config/0").unwrap()
     }
 
+    #[zbus(property)]
     fn ipv6_config(&self) -> ObjectPath<'_> {
         ObjectPath::try_from("/org/freedesktop/NetworkManager/IP6Config/0").unwrap()
     }
@@ -95,48 +123,24 @@ struct IP4Config;
 
 #[interface(name = "org.freedesktop.NetworkManager.IP4Config")]
 impl IP4Config {
+    #[zbus(property)]
     fn addresses(&self) -> Vec<(u32, u32, ObjectPath<'_>)> {
         Vec::new()
     }
 
-    fn gateways(&self) -> Vec<(i32, u32, ObjectPath<'_>)> {
-        Vec::new()
-    }
-
+    #[zbus(property)]
     fn nameservers(&self) -> Vec<u32> {
         Vec::new()
     }
 
+    #[zbus(property)]
     fn domains(&self) -> Vec<&str> {
         Vec::new()
     }
 
-    fn wins_servers(&self) -> Vec<u32> {
-        Vec::new()
-    }
-
-    fn routes(&self) -> Vec<(u32, u32, u32, i32, ObjectPath<'_>)> {
-        Vec::new()
-    }
-
-    fn gateway(&self) -> u32 {
-        0
-    }
-
-    fn method(&self) -> u32 {
-        2
-    }
-
-    fn is_gateway(&self) -> bool {
-        false
-    }
-
-    fn dns_servers(&self) -> Vec<u32> {
-        Vec::new()
-    }
-
-    fn dns_search(&self) -> Vec<&str> {
-        Vec::new()
+    #[zbus(property)]
+    fn gateway(&self) -> &str {
+        "172.17.0.1"
     }
 }
 
@@ -144,48 +148,24 @@ struct IP6Config;
 
 #[interface(name = "org.freedesktop.NetworkManager.IP6Config")]
 impl IP6Config {
+    #[zbus(property)]
     fn addresses(&self) -> Vec<(Vec<u8>, u32, i32, ObjectPath<'_>)> {
         Vec::new()
     }
 
-    fn gateways(&self) -> Vec<(i32, u32, ObjectPath<'_>)> {
-        Vec::new()
-    }
-
+    #[zbus(property)]
     fn nameservers(&self) -> Vec<Vec<u8>> {
         Vec::new()
     }
 
+    #[zbus(property)]
     fn domains(&self) -> Vec<&str> {
         Vec::new()
     }
 
-    fn routes(&self) -> Vec<(Vec<u8>, u32, Vec<u8>, i32, ObjectPath<'_>)> {
-        Vec::new()
-    }
-
-    fn gateway(&self) -> Vec<u8> {
-        Vec::new()
-    }
-
-    fn method(&self) -> u32 {
-        2
-    }
-
-    fn is_gateway(&self) -> bool {
-        false
-    }
-
-    fn dns_servers(&self) -> Vec<Vec<u8>> {
-        Vec::new()
-    }
-
-    fn dns_search(&self) -> Vec<&str> {
-        Vec::new()
-    }
-
-    fn dhcp_iaid(&self) -> u32 {
-        0
+    #[zbus(property)]
+    fn gateway(&self) -> &str {
+        ""
     }
 }
 
@@ -193,38 +173,37 @@ struct WiredDevice;
 
 #[interface(name = "org.freedesktop.NetworkManager.Device")]
 impl WiredDevice {
-    fn ip4_addresses(&self) -> Vec<(u32, u32, ObjectPath<'_>)> {
-        Vec::new()
-    }
-
+    #[zbus(property)]
     fn ip4_config(&self) -> ObjectPath<'_> {
         ObjectPath::try_from(IP4_CONFIG_PATH).unwrap()
     }
 
-    fn ip6_addresses(&self) -> Vec<(Vec<u8>, u32, i32, ObjectPath<'_>)> {
-        Vec::new()
-    }
-
+    #[zbus(property)]
     fn ip6_config(&self) -> ObjectPath<'_> {
         ObjectPath::try_from(IP6_CONFIG_PATH).unwrap()
     }
 
+    #[zbus(property)]
     fn managed(&self) -> bool {
         true
     }
 
+    #[zbus(property)]
     fn name(&self) -> &str {
         "eth0"
     }
 
+    #[zbus(property)]
     fn state(&self) -> u32 {
         100
     }
 
+    #[zbus(property)]
     fn type_(&self) -> u32 {
         1
     }
 
+    #[zbus(property)]
     fn active_connection(&self) -> ObjectPath<'_> {
         ObjectPath::try_from(ACTIVE_CONNECTION_PATH).unwrap()
     }
